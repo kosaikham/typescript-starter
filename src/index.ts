65e4f4aa6089360656ac7c02f17248370e34366f
Sai Lao Kham <saikham.email@gmail.com>
2023-10-21 12:56:54 +0700

    add comments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * @summary this function accepts 2 number-typed parameters, do add operation and return the total sum of them
+ * @param a number
+ * @param b number
+ * @returns number
+ */
 function add(a: number, b: number): number {
   return a + b;
 }
